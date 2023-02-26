7e0b0492ce7cf356c5ed2a93a354588ea311a115
Nick Trevino <nicknbt25@gmail.com>
2023-02-26 15:10:11 -0500

    test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,1 @@
-if (process.env.NODE_ENV === 'development') { // Or, `process.env.NODE_ENV !== 'production'`
-    // Only runs in development and will be stripped from production build.
-}
+// Silence is Golden
